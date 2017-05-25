d61f7821ae442eb27ea1cc27406b221a7199b309
Yoni Teitelbaum <ytbaum@gmail.com>
2017-05-25 19:53:37 +0300

    added comments

--- helper_functions.R
+++ helper_functions.R
@@ -9,6 +9,10 @@ get.locus <- function(locus.name) {
   return(strsplit(locus.name, "-")[[1]][2])
 }
 
+# function to extract the rows having the given alleles in the given loci
+# from the probabilities data frame
+# probs: a data-frame containing probabilities
+# alleles: a one-row data-frame containing a list of which alleles appear at which loci
 get.prob.rows <- function(probs, alleles) {
   output.df <- data.frame()
   for (locus.name in names(alleles)){

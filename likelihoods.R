7b16b754d2691d03ed8c7cd31e2b99c86c591c31
Yoni Teitelbaum <ytbaum@gmail.com>
2017-05-25 19:52:55 +0300

    don't bring row names along when making new df, so that rows are numbered from 1

--- likelihoods.R
+++ likelihoods.R
@@ -3,13 +3,13 @@ library(dplyr)
 
 possibilities.raw <- read.csv("Assignment a la Gili_sheet1.csv", stringsAsFactors = FALSE,
                               header = FALSE)
-possibilities <- data.frame(possibilities.raw[11:164, c(1:13)])
+possibilities <- data.frame(possibilities.raw[11:164, c(1:13)], row.names = NULL)
 colnames(possibilities) <- possibilities.raw[10,c(1:13)]
 
 probabilities.raw <- read.csv("Assignment a la Gili_sheet2_modified.csv",
                               stringsAsFactors = FALSE,
                               header = FALSE)
-probabilities <- data.frame(probabilities.raw[4:135,1:10])
+probabilities <- data.frame(probabilities.raw[4:135,1:10], row.names = NULL)
 colnames(probabilities) <- as.character(probabilities.raw[3, 1:10])
 
 # check that probabilities sum to 1

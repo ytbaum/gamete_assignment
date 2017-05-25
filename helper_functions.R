bb2e8a741103cdc688e1c3332d9f54a47389f431
Yoni Teitelbaum <ytbaum@gmail.com>
2017-05-25 18:44:32 +0300

    functionality to extract only the rows having the given alleles at the given loci

--- helper_functions.R
+++ helper_functions.R
@@ -1,4 +1,19 @@
 is.possibility <- function(row) {
   length(row["Mother"]) == 0
 }
-  
+
+get.locus <- function(locus.name) {
+  return(strsplit(locus.name, "-")[[1]][2])
+}
+
+get.prob.rows <- function(probs, alleles) {
+  output.df <- data.frame()
+  for (locus.name in names(alleles)){
+    locus <- get.locus(locus.name)
+    allele <- as.character(alleles[locus.name])
+    row <- probs[which(probs$Locus == locus & probs$Allele == allele),]
+    output.df <- rbind(output.df, row)
+  }
+
+  return(output.df)
+}
